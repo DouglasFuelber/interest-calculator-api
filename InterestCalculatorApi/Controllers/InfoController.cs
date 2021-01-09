2097eebbe1ffcc030c4fc68d8519bc3eb05bfa23
Douglas Fuelber <contato@douglasfuelber.com>
2021-01-09 19:30:24 -0300

    Added showmethecode endpoint

---
+++ InterestCalculatorApi/Controllers/InfoController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InterestCalculatorApi.Controllers
+{
+    [ApiController]
+    [Route("showmethecode")]
+    public class InfoController : Controller
+    {
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return NotFound();
+        }
+    }
+}
