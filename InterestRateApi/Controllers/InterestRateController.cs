a9add94b15ba283d1a5f374fc2858ca64c821e48
Douglas Fuelber <contato@douglasfuelber.com>
2021-01-09 14:12:47 -0300

    Created interest rate api project

---
+++ InterestRateApi/Controllers/InterestRateController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace InterestRateApi.Controllers
+{
+    [ApiController]
+    [Route("taxajuros")]
+    public class InterestRateController : ControllerBase
+    {
+
+        private readonly ILogger<InterestRateController> _logger;
+
+        public InterestRateController(ILogger<InterestRateController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public double Get()
+        {
+            return 0.01;
+        }
+    }
+}
