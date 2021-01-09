6068c859b8755513296016ffa55bd0ea50d7266f
Douglas Fuelber <contato@douglasfuelber.com>
2021-01-09 18:20:47 -0300

    Fixed dependency injection issue

--- InterestRateApi/Startup.cs
+++ InterestRateApi/Startup.cs
@@ -1,15 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using InterestRateApi.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 
 namespace InterestRateApi
@@ -28,6 +22,7 @@ namespace InterestRateApi
         {
 
             services.AddControllers();
+            services.AddScoped<IInterestRateService, InterestRateService>();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "InterestRateApi", Version = "v1" });
