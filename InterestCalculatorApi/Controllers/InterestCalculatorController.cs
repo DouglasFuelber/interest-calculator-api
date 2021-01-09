6e8354dca89b5824306449f344bbdbebdc17c74e
Douglas Fuelber <contato@douglasfuelber.com>
2021-01-09 19:12:06 -0300

    Added InterestCalculatorApi project

---
+++ InterestCalculatorApi/Controllers/InterestCalculatorController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InterestCalculatorApi.Controllers
+{
+    [ApiController]
+    [Route("calculajuros")]
+    public class InterestCalculatorController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<double> Get()
+        {
+            return NotFound();
+        }
+    }
+}
