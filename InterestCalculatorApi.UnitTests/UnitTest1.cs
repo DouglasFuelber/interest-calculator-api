19d92ec711ffa9efab6a5fdfcd385953ffea83d7
Douglas Fuelber <contato@douglasfuelber.com>
2021-01-09 19:14:11 -0300

    Added InterestCalculatorApi UnitTests project

---
+++ InterestCalculatorApi.UnitTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace InterestCalculatorApi.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
