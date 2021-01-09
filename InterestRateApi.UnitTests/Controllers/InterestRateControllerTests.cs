99e3ee786dccdc721a28df2c611b908b924a921a
Douglas Fuelber <contato@douglasfuelber.com>
2021-01-09 17:54:10 -0300

    Added InterestRateApiUnitTests project

---
+++ InterestRateApi.UnitTests/Controllers/InterestRateControllerTests.cs
@@ -0,0 +1,24 @@
+﻿using InterestRateApi.Controllers;
+using InterestRateApi.Services;
+using Xunit;
+
+namespace InterestRateApi.UnitTests.Controllers
+{
+    public class InterestRateControllerTests
+    {
+        InterestRateController _controller;
+        IInterestRateService _service;
+        public InterestRateControllerTests()
+        {
+            _service = new InterestRateService();
+            _controller = new InterestRateController(_service);
+        }
+
+        [Fact]
+        public void Get_WhenCalled_ReturnsDouble()
+        {
+            var getResult = _controller.Get();
+            Assert.IsType<double>(getResult);
+        }
+    }
+}
---
+++ InterestRateApi.UnitTests/Services/InterestRateServiceTests.cs
@@ -0,0 +1,21 @@
+﻿using InterestRateApi.Services;
+using Xunit;
+
+namespace InterestRateApi.UnitTests.Services
+{
+    public class InterestRateServiceTests
+    {
+        IInterestRateService _service;
+        public InterestRateServiceTests()
+        {
+            _service = new InterestRateService();
+        }
+
+        [Fact]
+        public void GetInterestRate_WhenCalled_ReturnsInterestRate()
+        {
+            var interestRate = _service.GetInterestRate();
+            Assert.Equal(0.01, interestRate);
+        }
+    }
+}
