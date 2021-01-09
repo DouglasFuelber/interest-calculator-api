b969938479e6cbdeb7e427c29ae511f2deeb53f6
Douglas Fuelber <contato@douglasfuelber.com>
2021-01-09 20:00:55 -0300

    Created unit test for calculajuros endpoint

---
+++ InterestCalculatorApi.UnitTests/Controllers/InterestCalculatorControllerTests.cs
@@ -0,0 +1,31 @@
+﻿using InterestCalculatorApi.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Xunit;
+
+namespace InterestCalculatorApi.UnitTests.Controllers
+{
+    public class InterestCalculatorControllerTests
+    {
+        InterestCalculatorController _controller;
+
+        public InterestCalculatorControllerTests()
+        {
+            _controller = new InterestCalculatorController();
+        }
+
+        [Fact]
+        public void Get_WhenCalled_ReturnsOkResult()
+        {
+            var getResult = _controller.Get(0, 0);
+            Assert.IsType<OkObjectResult>(getResult.Result);
+        }
+
+        [Theory]
+        [InlineData(100, 5, 105.10)]
+        public void Get_WhenCalled_ReturnsInterestCalculated(double valorinicial, int meses, int jurosResultado)
+        {
+            var getResult = (OkObjectResult) _controller.Get(valorinicial, meses).Result;
+            Assert.Equal(jurosResultado, getResult.Value);
+        }
+    }
+}
