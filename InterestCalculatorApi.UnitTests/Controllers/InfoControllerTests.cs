582c82c12c9d03f2c7805013e5c2b4db207ae041
Douglas Fuelber <contato@douglasfuelber.com>
2021-01-09 19:31:16 -0300

    Created unit test to showmethecode endpoint

---
+++ InterestCalculatorApi.UnitTests/Controllers/InfoControllerTests.cs
@@ -0,0 +1,30 @@
+﻿using InterestCalculatorApi.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Xunit;
+
+namespace InterestCalculatorApi.UnitTests.Controllers
+{
+    public class InfoControllerTests
+    {
+        InfoController _controller;
+
+        public InfoControllerTests()
+        {
+            _controller = new InfoController();
+        }
+
+        [Fact]
+        public void Get_WhenCalled_ReturnsOkResult()
+        {
+            var getResult = _controller.Get();
+            Assert.IsType<OkObjectResult>(getResult.Result);
+        }
+
+        [Fact]
+        public void Get_WhenCalled_ReturnsRepositoryUrl()
+        {
+            var getResult = _controller.Get();
+            Assert.Equal("https://github.com/DouglasFuelber/interest-calculator-api", getResult.Value);
+        }
+    }
+}
